c2a36890fea460693da469de157c4850f5b6122e
hyojin-k <hyo.khj@gmail.com>
2021-10-12 18:27:21 +0900

    edit-postcard

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,27 +1,46 @@
 import React from "react";
-import { Grid, Image, Text } from '../elements';
+import { Grid, Image, Text } from "../elements";
+import { FaUserCircle } from 'react-icons/fa';
+import { MdDateRange} from 'react-icons/md';
 
-import {history} from '../redux/configuerStore';
+import { history } from "../redux/configuerStore";
 
 const Post = () => {
   return (
     <>
-        <Grid width='900px'  border=' 3px solid #f5df4d' borderRadius='50px' padding='30px 50px' margin='auto'>
-            <Grid is_flex >
-                <Grid is_flex width='auto'>
-                <Text bold size='16px'>Hwang</Text>
-                </Grid>
-                <Grid is_flex width='auto'>
-                <Text bold size='24px'>오징어 게임</Text>
-                </Grid>
-                <Grid is_flex width='auto'>
-                <Text size='16px'>2021.10.10</Text>
-                </Grid>
-            </Grid>
-            <Image shape='rectangle'/>
+      <Grid
+        width="65%" 
+        border=" 1px solid #f5df4d"
+        borderRadius="2px"
+        margin="auto"
+        shadow=' 2px 2px 2px #dbdbdb'
+      >
+        <Grid is_flex>
+          <Grid is_flex width="auto">
+              <FaUserCircle style={{width: "24px", height: "24px", color: "#939597", margin:'0 10px'}}/>
+            <Text color='#939597' bold size="16px">
+              Hwang
+            </Text>
+          </Grid>
+          <Grid is_flex width="auto">
+            <Text color='#939597' size="16px">2021.10.10</Text>
+            <MdDateRange style={{width: "24px", height: "24px", color: "#939597", margin:'0 10px'}}/>
+          </Grid>
         </Grid>
+
+        <Grid padding="0">
+          <Image shape="rectangle" />
+        </Grid>
+        <Grid is_flex>
+          <Grid is_flex>
+            <Text color='#939597' bold size="24px" margin=' 20px auto'>
+              폰트를 바꿔보자~
+            </Text>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
-    );
+  );
 };
 
 export default Post;
--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -2,15 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const Grid = (props) => {
-  const { is_flex, width, margin, padding, children, border, borderRadius, center, _onClick } = props;
+  const { is_flex, width, margin, padding, children, bg, border, borderRadius, shadow, center, _onClick } = props;
 
   const styles = {
     is_flex: is_flex,
     width: width,
     margin: margin,
     padding: padding,
+    bg: bg, 
     border: border,
     borderRadius:borderRadius,
+    shadow:shadow,
     center: center,
   };
   return (
@@ -30,6 +32,7 @@ Grid.defaultProps = {
   bg: false,
   border: false,
   borderRadius: false,
+  shadow: false,
   center: false,
   _onClick: () => {},
   children: null,
@@ -40,8 +43,10 @@ const GridBox = styled.div`
   /* margin: auto; */
 
   width: ${(props) => props.width};
+  ${(props) => (props.bg ? `background-color: ${props.bg};` : "")};
   ${(props) => (props.border ? `border: ${props.border};` : "")};
   ${(props) => (props.borderRadius ? `border-radius: ${props.borderRadius};` : "")};
+  ${(props) => (props.shadow ? `box-shadow: ${props.shadow};` : "")};
   ${(props) => (props.padding ? `padding: ${props.padding};` : "")};
   ${(props) => (props.margin ? `margin: ${props.margin};` : "")};
 
