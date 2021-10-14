a5eb51efff42afc8c693ed797cd365f7bc0f9bf0
hyojin-k <hyo.khj@gmail.com>
2021-10-14 11:44:10 +0900

    edit mypage

--- src/components/MypagePost.js
+++ src/components/MypagePost.js
@@ -2,98 +2,48 @@
 
 import React from "react";
 import styled from "styled-components";
+import { IoIosArrowForward } from "react-icons/io"; 
 
-<<<<<<< HEAD
-// import { Image } from "../elements";
-=======
-import { Image, Grid } from "../elements";
->>>>>>> 987bb8ff4c7c1070ff34502065250c6e510414d9
 import { history } from "../redux/configuerStore";
 
-const MypagePost = props => {
-  console.log(props);
-  const { id, image_url } = props;
+const MypagePost = (props) => {
+  const { url, postId, title } = props;
 
   return (
     <>
-      <Grid is_flex margin="20px" width="auto">
-        <Post>
-          <PostLink
-            onClick={() => {
-              history.push(`/detail/${id}`);
-            }}
-          >
-<<<<<<< HEAD
-            <PostImage
-              src={`https://img.youtube.com/vi/${url}/sddefault.jpg`}
-=======
-            <Image
-              shape="rectangle"
-              src={image_url}
-              style={{ width: "250px", height: "250px" }}
->>>>>>> 987bb8ff4c7c1070ff34502065250c6e510414d9
-            />
-            <TitleWrap>
-              <Title>hover</Title>
-            </TitleWrap>
-          </PostLink>
-        </Post>
-      </Grid>
+      <Post>
+        <PostLink
+          onClick={() => {
+            history.push(`/detail/${postId}`);
+          }}
+        >
+          <PostImage
+            src={`https://img.youtube.com/vi/${url}/sddefault.jpg`}
+          />
+          <TitleWrap>
+            <Title>
+              {title}
+              <Link>
+                <span style={{fontSize: '16px', verticalAlign:'top'}}>바로가기</span> 
+                <IoIosArrowForward style={{ marginLeft: '8px'}}/>
+              </Link>
+            </Title>
+          </TitleWrap>
+        </PostLink>
+      </Post>
     </>
   );
 };
 
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: 250px 250px 250px;
-  grid-template-rows: 250px 250px 250px;
-  gap: 3%;
-  border: 1px solid red;
-`;
 
 const Post = styled.div`
   display: flex;
   height: 100%;
   box-sizing: border-box;
-  /* position: relative;
-  overflow: hidden;
-  color: #ffffff;
-  font-size: 20px;
-  font-weight: bold;
-  background-color:#000;
-  background-size: cover;
   cursor: pointer;
 
-  -webkit-transition: all 0.35s ease;
-  transition: all 0.35s ease; */
-
-  /* background-color: pink; */
-  /* background-image: url("https://img.seoul.co.kr/img/upload/2021/09/28/SSI_20210928100517.jpg"); */
-
-
-  /* &:after{
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.65);
-    -webkit-transition: all 0.35s ease;
-    transition: all 0.35s ease;
-    opacity: 0;
-  }
-
-  &:hover:after{
-    opacity: 1;
-    position: absolute;
-    top: 10px;
-    bottom: 10px;
-    left: 10px;
-    right: 10px;
-  } */
-
-  &:hover {
-    /* -webkit-transition: all 300ms ease-in-out;
+  /* &:hover {
+    -webkit-transition: all 300ms ease-in-out;
     -o-transition: all 300ms ease-in-out;
     transition: all 300ms ease-in-out;
     -webkit-filter: blur(1px);
@@ -101,8 +51,8 @@ const Post = styled.div`
     -ms-filter: blur(1px);
     -o-filter: blur(1px);
     filter: blur(1px);
-    transform: scale(1.03); */
-  }
+    transform: scale(1.03);
+  } */
 `;
 
 const PostLink = styled.a`
@@ -112,6 +62,7 @@ const PostLink = styled.a`
   width: 100%;
   color:#fff;
   font-size: 20px;
+  font-weight: bold;
   background-color: #000;
   box-sizing: border-box;
   -webkit-transition: all 0.35s ease;
@@ -123,15 +74,20 @@ const PostLink = styled.a`
     bottom: 0;
     left: 0;
     right: 0;
+    content: '';
     background-color: rgba(0, 0, 0, 0.65);
     -webkit-transition: all 0.35s ease;
     transition: all 0.35s ease;
     opacity: 0;
-
-    &:hover:after{
-      
-    }
-
+  }
+  &:hover:after{
+    opacity: 1;
+    position: absolute;
+    /* top: 10px;
+    bottom: 10px;
+    left: 10px;
+    right: 10px; */
+  }
 `;
 
 const PostImage = styled.img`
@@ -156,16 +112,24 @@ const TitleWrap = styled.div`
   left: 0;
   right: 0;
   z-index: 1;
-  padding: 40px;
+  text-align: center;
 `
 const Title = styled.p`
-  /* width:100%;
-  height: 100%; */
-  /* &:hover{
+  width:90%;
+  height: 100%;
+  padding-top: 70px;
+  margin: 0 5%;
+  word-break: keep-all;
+  opacity: 0;
+
+  &:hover{
     -webkit-transform: translate(0px, 0px);
     transform: translate(0px, 0px);
     opacity: 1;
-  } */
+  }
+`
+const Link = styled.p`
+  padding-top: 20px;
 `
 
 export default MypagePost;
