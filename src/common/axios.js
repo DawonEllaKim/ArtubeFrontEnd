408efa80ce19a076d27963d1ca8c283117af5e9f
DawonEllaKim <dawonella0411@gmail.com>
2021-10-15 12:35:11 +0900

    update

--- src/common/axios.js
+++ src/common/axios.js
@@ -4,13 +4,13 @@ const instance = axios.create({
   // 기본적으로 우리가 바라볼 서버의 주소
 
   // 성규님 주소
-  baseURL: "http://3.34.95.37",
+  // baseURL: "http://3.34.95.37",
 
   // 유정님 주소
   // baseURL: "http://3.35.141.41",
 
   //제이슨 서버
-  // baseURL: "http://localhost:4000",
+  baseURL: "http://localhost:4000",
   headers: {
     "content-type": "application/json;charset=UTF-8",
     accept: "application/json",
